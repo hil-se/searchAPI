3e45b12ef9f81295dc9f6d7e58af2657b26bb9f5
therealadarsh <adarshbalakrishnan88@gmail.com>
2020-12-11 06:21:46 -0500

    Minor changes to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 with open("README.md") as f:
 	read_description = f.read()
 
 
 setup(
-	name = 'SearchAPI',
-	version = '0.1',
-	packages = ['API',],
-	long_description = read_description
-	)
+	name = "searchscraper",
+	version = "0.1",
+	author = "Adarsh Balakrishnan, Zhe Yu",
+	author_email = "adarshbalakrishnan88@gmail.com",
+	description = ("A scholar search API that can be used to scrape data from Arxiv."),
+	long_description = read_description,
+	long_description_content_type="text/markdown",
+	url = 'https://github.com/hil-se/searchAPI',
+	packages=find_packages(),
+	classifiers = [
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Text Processing :: Markup :: LaTeX",
+        ]
+	)        
+
